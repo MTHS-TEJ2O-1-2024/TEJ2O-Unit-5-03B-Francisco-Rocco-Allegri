dc71a9e14536a0d857b4de6c298e8ea994511c36
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-09-27 10:54:49 -0400

    changed comment and function

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Francisco
+ * Created on: Sep 2024
+ * This program makes a physical LED turn on
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
